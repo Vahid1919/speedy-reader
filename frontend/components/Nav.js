13c030ced621689a1363430bc69c412b52222b2f
Vahid <noreply+vahid@objectbox.io>
2022-10-10 20:53:50 +0200

    Separating CSS into modules

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,10 +1,11 @@
 import Link from "next/link"
+import styles from "../styles/Nav.module.css"
 
 const Nav = () => {
   return (
     <Link href="/dashboard">
-      <div className="nav">
-          <a className="logo-title disable-select">Speedy Reader</a>
+      <div className={styles.nav}>
+          <a className={`${styles.title} disable-select`}>Speedy Reader</a>
       </div>
     </Link>
     
--- frontend/components/Slider.js
+++ frontend/components/Slider.js
@@ -1,15 +1,17 @@
+import styles from "../styles/Slider.module.css"
+
 const Slider = ({wpm, setWpm}) => {
 
  
 
 
   return (
-    <div className='slider-container'>
-    <input type="range" id="wpm" name="volume"
+    <div className={styles.sliderContainer}>
+    <input className={styles.rangeInput} type="range" id="wpm" name="volume"
            min="0" max="700" step={50} defaultValue='200' onChange={(e) => {setWpm(e.target.value)}}/>
-    <span className="slider-text-container">
-      <label id="wpm-text" className="slider-text disable-select" htmlFor="wpm">{wpm}</label>
-      <div className="wpm disable-select">WPM</div>
+    <span className={styles.sliderTextContainer}>
+      <label id="wpm-text" className={`${styles.sliderText} disable-select`} htmlFor="wpm">{wpm}</label>
+      <div className={`${styles.wpm} disable-select`}>WPM</div>
     </span>
    
   </div>
